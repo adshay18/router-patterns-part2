5e8825a1583c0e2ac2f3238621c9223430d4821a
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-07 07:48:32 -0400

    make colorlist and routes functional

--- src/ColorIndex.js
+++ src/ColorIndex.js
@@ -1,7 +1,26 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
 
-const ColorIndex = () => {
-	return <h1>ColorIndex Page</h1>;
+const ColorIndex = ({ colors }) => {
+	return (
+		<div className="ColorIndex">
+			<div className="ColorIndex-header">
+				<h2>Welcome to the color factory.</h2>
+			</div>
+			<div className="ColorIndex-list">
+				<p>Please select a color:</p>
+				{colors.map((color) => (
+					<Link key={uuid()} to={`/colors/${color}`}>
+						{color}
+					</Link>
+				))}
+			</div>
+		</div>
+	);
 };
 
+ColorIndex.defaultProps = {
+	colors: [ 'red', 'green', 'blue' ]
+};
 export default ColorIndex;

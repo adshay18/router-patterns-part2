ea098961d08e6754fc58c4be7ef20f687c24d46d
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-07 10:43:21 -0400

    style landing page

--- src/ColorIndex.js
+++ src/ColorIndex.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
+import './ColorIndex.css';
 
 const ColorIndex = ({ colors }) => {
 	const links = colors.map((color) => (
